940dd65b0b0db3ac6b9733e132af70946ea1ae3b
Hannes Staffler <hannes.staffler@sengaro.com>
2014-12-05 01:57:26 +0100

    added cron, added env_vars

--- cookbooks/cron/attributes/cron.rb
+++ cookbooks/cron/attributes/cron.rb
@@ -1,4 +1,8 @@
 # Add one hash per cron job required
 # Set the utility instance name to install each cron job on via instance_name
 
-default[:custom_crons] = [{:name => "snowflys", :time => "45 4 * * *", :command => "cd /data/looptown/current && /usr/local/bin/bundle exec rake products:parser:snowflys SHOP_ID=1 RAILS_ENV=production REPORT=email", :instance_name => "cron"}]
+# 0 * * * *
+
+# 45 4 * * *
+
+default[:custom_crons] = [{:name => "snowflys", :time => "2 * * * *", :command => "cd /data/looptown/current && /usr/local/bin/bundle exec rake products:parser:snowflys SHOP_ID=1 RAILS_ENV=production REPORT=email", :instance_name => "utility"}]
--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -83,7 +83,7 @@ include_recipe "cron"
 
 #uncomment to set environment variables in passenger or unicorn
 # Set environment variables as specified in cookbooks/env_vars/attributes/env_vars.rb
-#include_recipe "env_vars"
+include_recipe "env_vars"
 
 
 #uncomment to include the mysql_replication_check recipe
